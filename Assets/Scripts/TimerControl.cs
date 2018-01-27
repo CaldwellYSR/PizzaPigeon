801bc681bb30c7b0d76b276440b66419a69c64b3
Cody McPheron <acmcpheron@gmail.com>
2018-01-27 15:10:40 -0500

    added game over condition to running out of time.

--- Assets/Scripts/TimerControl.cs
+++ Assets/Scripts/TimerControl.cs
@@ -1,12 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class TimerControl : MonoBehaviour
 {
 	public int timeLeft = 5;
 	public Text PigeonTimer;
 	public Text PersonTimer;
+	public string NextLevel;
 
 	// Use this for initialization
 	void Start()
@@ -17,14 +19,18 @@ public class TimerControl : MonoBehaviour
 	// Update is called once per frame
 	void Update()
 	{
-		PigeonTimer.text = ("" + timeLeft);
-		PersonTimer.text = ("" + timeLeft);
 
-		if (timeLeft <= 0)
-		{
-			StopCoroutine("LoseTime");
+		if (timeLeft <= 0) {
 			PigeonTimer.text = "Times Up!";
 			PersonTimer.text = "Times Up!";
+
+			if (timeLeft <= -3) {
+				StopCoroutine ("LoseTime");
+				SceneManager.LoadScene (NextLevel);
+			}
+		} else {
+			PigeonTimer.text = ("" + timeLeft);
+			PersonTimer.text = ("" + timeLeft);
 		}
 	}
 
