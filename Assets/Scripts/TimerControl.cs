c48a713f3ca6d64878ffd2e92700a60a85b8cb93
Cody McPheron <acmcpheron@gmail.com>
2018-01-27 14:57:33 -0500

    timer now works and updates properly on both sides of the screen.

---
+++ Assets/Scripts/TimerControl.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TimerControl : MonoBehaviour
+{
+	public int timeLeft = 5;
+	public Text PigeonTimer;
+	public Text PersonTimer;
+
+	// Use this for initialization
+	void Start()
+	{
+		StartCoroutine("LoseTime");
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		PigeonTimer.text = ("" + timeLeft);
+		PersonTimer.text = ("" + timeLeft);
+
+		if (timeLeft <= 0)
+		{
+			StopCoroutine("LoseTime");
+			PigeonTimer.text = "Times Up!";
+			PersonTimer.text = "Times Up!";
+		}
+	}
+
+	IEnumerator LoseTime()
+	{
+		while (true)
+		{
+			yield return new WaitForSeconds(1);
+			timeLeft--;
+		}
+	}
+}
