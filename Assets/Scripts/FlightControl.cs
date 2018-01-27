ea1aa2c6440b0de623befb612c755dd3b83479d1
Matthew Caldwell <mcaldwell@derbywars.com>
2018-01-26 23:02:57 -0500

    Add constant forward flight

---
+++ Assets/Scripts/FlightControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlightControl : MonoBehaviour {
+
+  [Range(1, 6)]
+  public float Speed;
+
+  [Range(10, 100)]
+  public float MaxSpeed;
+
+  private Rigidbody Body;
+  private Vector3 Direction;
+
+  void Start() {
+    Body = GetComponent<Rigidbody>();
+    Direction = transform.forward;
+  }
+
+	void FixedUpdate () {
+    Body.AddForce(Direction * Speed);
+
+    if (Body.velocity.magnitude >= MaxSpeed) {
+      Body.velocity = Body.velocity.normalized * MaxSpeed;
+    }
+	}
+}
