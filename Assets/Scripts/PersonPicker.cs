6de88e68552df01856fb172592f68842791a7f47
Cody McPheron <acmcpheron@gmail.com>
2018-01-27 22:13:10 -0500

    used import asset to add person prefab to existing master.

---
+++ Assets/Scripts/PersonPicker.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonPicker : MonoBehaviour {
+
+	public List<Sprite> Peoples;
+
+	private SpriteRenderer Sprite;
+
+	void Start() {
+		Sprite = GetComponent<SpriteRenderer> ();
+		int selected = Random.Range (0, Peoples.Count);
+		Sprite.sprite = Peoples[selected];
+	}
+
+}
